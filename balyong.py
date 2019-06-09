21f505fd8e6299ea28e545674309f9dc1ecc2290
rlackdgus09 <51521024+rlackdgus09@users.noreply.github.com>
2019-06-09 10:39:22 +0900

    Update balyong.py

--- balyong.py
+++ balyong.py
@@ -13,7 +13,6 @@ async def on_ready():
     print('===============')
     activity = discord.Game(name="슈발뇽마음속♡♡")
     await client.change_presence(status=discord.Status.idle, activity=activity)
-    await message.channel.send('짜짠') 
 
 @client.event
 async def on_message(message):
